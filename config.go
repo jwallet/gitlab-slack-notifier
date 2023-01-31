b82f8dffe9d868913709e9033d53b1b322480b7f
Jwallet <wizik20@gmail.com>
2023-01-31 07:26:03 -0500

    env

--- config.go
+++ config.go
@@ -4,17 +4,13 @@ import "os"
 
 type Environment string
 
-// Constants
-
-var USER_EMAIL_SPACE_REPLACER = "."
-
-var SLACK_BOT_NOTIFICATION_COLOR = "#0099CC"
-var SLACK_BOT_NOTIFICATION_GREATINGS = "You were mentionned on GitLab"
-
-// Environment variables
-
 var PORT int = getEnvInt("PORT") // optional, default 3000
 
-var SLACK_BOT_OAUTH_TOKEN string = os.Getenv("SLACK_BOT_OAUTH_TOKEN")   // required, bot token xoxb-12345678-12345678
+var SLACK_BOT_NOTIFICATION_COLOR = os.Getenv("SLACK_BOT_NOTIFICATION_COLOR")         // optional, color of the notification border, "#0099CC"
+var SLACK_BOT_NOTIFICATION_GREATINGS = os.Getenv("SLACK_BOT_NOTIFICATION_GREATINGS") // required, message sent by the bot, "You were mentionned on GitLab"
+
+var SLACK_BOT_OAUTH_TOKEN string = os.Getenv("SLACK_BOT_OAUTH_TOKEN")   // required, bot token, xoxb-12345678-12345678
 var SLACK_BOT_READ_CHANNEL string = os.Getenv("SLACK_BOT_READ_CHANNEL") // optional, monitor one channel "CH1234567"
-var USER_EMAIL_DOMAIN string = os.Getenv("USER_EMAIL_DOMAIN")           // "gmail.com"
+
+var USER_EMAIL_DOMAIN string = os.Getenv("USER_EMAIL_DOMAIN")          // required, email domain, "business.com"
+var USER_EMAIL_SPACE_REPLACER = os.Getenv("USER_EMAIL_SPACE_REPLACER") // optional, leave empty to replace spaces to nothing, "."
